7569d2bbbe5b680432609987d1d4dc2f4d5f2b9c
Q. Parker <ghostdancex@gmail.com>
2013-02-12 10:18:23 -0600

    tunneling via local proxy exit handler

--- src/main/java/org/lantern/LocalExitRequestProcessor.java
+++ src/main/java/org/lantern/LocalExitRequestProcessor.java
@@ -3,9 +3,10 @@ package org.lantern;
 import java.io.IOException;
 
 import java.net.URL;
+import java.net.URI;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.InetSocketAddress;
-import javax.net.ssl.SSLEngine;
 
 import org.jboss.netty.bootstrap.ClientBootstrap;
 import org.jboss.netty.channel.Channel;
@@ -19,6 +20,11 @@ import org.jboss.netty.channel.ExceptionEvent;
 import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
 import org.jboss.netty.channel.socket.ClientSocketChannelFactory;
 import org.jboss.netty.handler.codec.http.HttpRequest;
+import org.jboss.netty.handler.codec.http.HttpResponse;
+import org.jboss.netty.handler.codec.http.HttpResponseStatus;
+import org.jboss.netty.handler.codec.http.HttpVersion;
+import org.jboss.netty.handler.codec.http.DefaultHttpResponse;
+import org.jboss.netty.handler.codec.http.HttpMethod;
 import org.jboss.netty.handler.codec.http.HttpRequestEncoder;
 
 import org.lantern.HttpRequestProcessor;
@@ -48,25 +54,49 @@ public class LocalExitRequestProcessor implements HttpRequestProcessor {
             ChannelHandlerContext ctx, MessageEvent ev) {
         HttpRequest req = (HttpRequest)ev.getMessage();
         log.debug("processing request to: {}", req.getUri());
+        Boolean tunnel = false;
+        if(req.getMethod() == HttpMethod.CONNECT) {
+            tunnel = true;
+        }
         ChannelFuture channelFuture = openOutgoingChannel(
-            browserToProxyChannel, req
+            ctx, browserToProxyChannel, req, tunnel
         );
         return true;
     }
 
-    public ChannelFuture openOutgoingChannel(final Channel browserToProxyChannel,
-           final HttpRequest req) {
+    public ChannelFuture openOutgoingChannel(
+            final ChannelHandlerContext ctx, 
+            final Channel browserToProxyChannel, 
+            final HttpRequest req, final Boolean tunnel) {
 
         ClientBootstrap cb = new ClientBootstrap(
             clientSocketChannelFactory
         );
-        ChannelPipeline p = cb.getPipeline();
-        final LanternClientSslContextFactory sslFactory =
-            new LanternClientSslContextFactory(this.ksm);
-        SSLEngine engine = sslFactory.getClientContext().createSSLEngine();
-        engine.setUseClientMode(true);
-        //p.addLast("ssl", new SslHandler(engine));
-        p.addLast("encoder", new HttpRequestEncoder());
+        InetSocketAddress remoteAddress = this.uriToAddress(req.getUri());
+        log.debug("got remote address: {} {}", remoteAddress.getHostName(), remoteAddress.getPort());
+
+        final ChannelFuture connectFuture = cb.connect(remoteAddress);
+        ChannelPipeline p = connectFuture.getChannel().getPipeline();
+
+        if(tunnel) {
+            ctx.getPipeline().remove("decoder");
+            ctx.getPipeline().remove("handler");
+            ctx.getPipeline().addLast("handler", new SimpleChannelUpstreamHandler() {
+                @Override
+                public void messageReceived(ChannelHandlerContext ctx,
+                        MessageEvent ev) {
+                    log.debug("inbound msg rcvd on outbound channel");
+                    connectFuture.getChannel().write(ev.getMessage());
+                }
+
+                @Override
+                public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent ev) {
+                    log.debug("caught exception in exit outbound channel: {}", ev);
+                }
+            });
+        } else {
+            p.addLast("encoder", new HttpRequestEncoder());
+        }
         p.addLast(
             "handler", 
             new SimpleChannelUpstreamHandler() {
@@ -83,7 +113,6 @@ public class LocalExitRequestProcessor implements HttpRequestProcessor {
             }
         );
 
-        ChannelFuture connectFuture = cb.connect(this.uriToAddress(req.getUri()));
        
         connectFuture.addListener(new ChannelFutureListener() {
             @Override
@@ -92,7 +121,20 @@ public class LocalExitRequestProcessor implements HttpRequestProcessor {
                 if (connectFuture.isSuccess()) {
                     log.debug("connected via exit handler");
                     browserToProxyChannel.setReadable(true);
-                    connectFuture.getChannel().write(req);
+                    if(tunnel) {
+                        log.debug("sending 200 response to CONNECT request (start tunneling)");
+                        HttpResponse resp = new DefaultHttpResponse(
+                            HttpVersion.HTTP_1_0, HttpResponseStatus.OK
+                        );
+                        resp.setHeader("Proxy-agent", "LanternLocal/0.0");
+                        browserToProxyChannel.write(resp);
+                        // we need this encoder to send the 200,
+                        // after that, we need to drop it.
+                        ctx.getPipeline().remove("encoder");
+                    } else {
+                        log.debug("writing http request to dst");
+                        connectFuture.getChannel().write(req);
+                    }
                 } else {
                     log.debug("connect via exit handler failed");
                     browserToProxyChannel.close();
@@ -111,18 +153,22 @@ public class LocalExitRequestProcessor implements HttpRequestProcessor {
         log.debug("closing...");
     }
 
-    private InetSocketAddress uriToAddress(String uri) {
-        URL url;
+    private InetSocketAddress uriToAddress(String uriString) {
+        URI uri;
+        if(!uriString.substring(0, 4).matches("http")) {
+            uriString = "https://" + uriString;
+        }
+        log.debug("parsing uri: {}", uriString);
         try {
-            url = new URL(uri);
-        } catch(MalformedURLException e) {
-            log.error("malformed url: {}", uri);
+            uri = new URI(uriString);
+        } catch(URISyntaxException e) {
+            log.error("malformed uri: {}, {}", uriString, e);
             return null;
         }
-        String host = url.getHost();
-        Integer port = url.getPort();
+        String host = uri.getHost();
+        Integer port = uri.getPort();
         if(port < 0) {
-            if(url.getProtocol().matches("^https")) {
+            if(uri.getScheme().matches("^https")) {
                 port = 443;
             } else {
                 port = 80;
